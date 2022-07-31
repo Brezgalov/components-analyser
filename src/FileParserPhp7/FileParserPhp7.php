a45eaee3d524bba033d7d838dd14263be5825f84
Oleg Brezgalov <brezgalov.developer@gmail.com>
2022-07-31 16:54:51 +0300

    php7 parser

---
+++ src/FileParserPhp7/FileParserPhp7.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Brezgalov\ComponentsAnalyser\FileParserPhp7;
+
+use Brezgalov\ComponentsAnalyser\FileParser\FileParser;
+use Brezgalov\ComponentsAnalyser\FileParser\Models\FileParseResult;
+
+class FileParserPhp7 extends FileParser
+{
+    const TOKEN_NS_SEPARATOR = "T_NS_SEPARATOR";
+    const TOKEN_USE = "T_USE";
+    const TOKEN_STRING = "T_STRING";
+
+    public function parseFile(string $filePath)
+    {
+        $result = new FileParseResult();
+        $tokens = $this->tokenizeFile($filePath);
+
+        if ($tokens === false) {
+            $result->error = "File \"{$filePath}\" not found or unavailable";
+            return $result;
+        }
+
+        if (empty($tokens)) {
+            return $result;
+        }
+
+        $useToken = false;
+        $dependency = null;
+        foreach ($tokens as $tokenInfo) {
+            if (is_string($tokenInfo)) {
+                if ($useToken) {
+                    $result->useClasses[] = $dependency;
+
+                    $useToken = false;
+                    $dependency = null;
+                }
+
+                continue;
+            }
+
+            list($tokenCode, $tokenVal) = $tokenInfo;
+            $tokenName = token_name($tokenCode);
+
+            if ($tokenName == self::TOKEN_USE) {
+                $useToken = true;
+                continue;
+            }
+
+            if ($useToken && ($tokenName == self::TOKEN_STRING || $tokenName == self::TOKEN_NS_SEPARATOR)) {
+                $dependency .= $tokenVal;
+            }
+        }
+
+        return $result;
+    }
+}
---
+++ test/UnitTests/FileParserPhp7/FileParserPhp7Test.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Brezgalov\ComponentsAnalyser\UnitTests\FileParserPhp7;
+
+use Brezgalov\ComponentsAnalyser\FileParser\Models\IFileParseResult;
+use Brezgalov\ComponentsAnalyser\FileParserPhp7\FileParserPhp7;
+use Brezgalov\ComponentsAnalyser\UnitTests\BaseTestCase;
+
+/**
+ * Class FileParserPhp8Test
+ * @package Brezgalov\ComponentsAnalyser\UnitTests\FileParserPhp8
+ *
+ * @requires PHP < 8.0
+ */
+class FileParserPhp7Test extends BaseTestCase
+{
+    /**
+     * @covers \Brezgalov\ComponentsAnalyser\FileParser\FileParser::tokenizeFile
+     * @covers \Brezgalov\ComponentsAnalyser\FileParserPhp7\FileParserPhp7::parseFile
+     */
+    public function testParse()
+    {
+        $parser = new FileParserPhp7();
+
+        $result = $parser->parseFile(TEST_DIR . '/ExampleComponents/A/CompB.php');
+
+        $this->assertInstanceOf(IFileParseResult::class, $result);
+        $this->assertNotEmpty($result->getError());
+
+        $result = $parser->parseFile(TEST_DIR . '/ExampleComponents/B/empty.php');
+        $this->assertInstanceOf(IFileParseResult::class, $result);
+        $this->assertEmpty($result->getError());
+        $this->assertEmpty($result->getUseDependencies());
+
+        $result = $parser->parseFile(TEST_DIR . '/ExampleComponents/A/MyFolder/TriggerC.php');
+        $this->assertInstanceOf(IFileParseResult::class, $result);
+        $this->assertEmpty($result->getError());
+
+        $useDepends = $result->getUseDependencies();
+
+        $this->assertTrue(in_array("ExampleComponents\C\CompC", $useDepends));
+        $this->assertTrue(in_array("ExampleComponents\B\CompB", $useDepends));
+    }
+}
