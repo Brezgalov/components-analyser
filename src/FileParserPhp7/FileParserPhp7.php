83edc24810e76a7223eed76001adfa8cd59a1af0
Oleg Brezgalov <brezgalov.developer@gmail.com>
2022-08-19 15:35:07 +0300

    remove dead code

--- src/FileParserPhp7/FileParserPhp7.php
+++ src/FileParserPhp7/FileParserPhp7.php
@@ -7,12 +7,6 @@ use Brezgalov\ComponentsAnalyser\FileParser\Models\FileParseResult;
 
 class FileParserPhp7 extends FileParser
 {
-    const TOKEN_NS_SEPARATOR = "T_NS_SEPARATOR";
-    const TOKEN_USE = "T_USE";
-    const TOKEN_STRING = "T_STRING";
-    const TOKEN_NAMESPACE = "T_NAMESPACE";
-    const TOKEN_CLASS = "T_CLASS";
-
     /**
      * @return array
      */
@@ -22,73 +16,4 @@ class FileParserPhp7 extends FileParser
 
         ];
     }
-
-//    public function parseFile(string $filePath)
-//    {
-//        $result = new FileParseResult();
-//        $tokens = $this->tokenizeFile($filePath);
-//
-//        if ($tokens === false) {
-//            $result->error = "File \"{$filePath}\" not found or unavailable";
-//            return $result;
-//        }
-//
-//        if (empty($tokens)) {
-//            return $result;
-//        }
-//
-//        $useToken = false;
-//        $namespaceIsBuilding = false;
-//        $classNamePassed = false;
-//        $dependency = null;
-//        foreach ($tokens as $tokenInfo) {
-//            if (is_string($tokenInfo)) {
-//                if ($useToken) {
-//                    $result->useClasses[] = $dependency;
-//
-//                    $useToken = false;
-//                    $dependency = null;
-//                }
-//
-//                if ($namespaceIsBuilding) {
-//                    $namespaceIsBuilding = false;
-//                }
-//
-//                continue;
-//            }
-//
-//            list($tokenCode, $tokenVal) = $tokenInfo;
-//            $tokenName = token_name($tokenCode);
-//
-//            switch ($tokenName) {
-//                case self::TOKEN_USE: {
-//                    $useToken = true;
-//                    continue 2;
-//                }
-//                case self::TOKEN_NAMESPACE: {
-//                    $result->namespace = '';
-//                    $namespaceIsBuilding = true;
-//                    continue 2;
-//                }
-//                case self::TOKEN_CLASS: {
-//                    $classNamePassed = true;
-//                    continue 2;
-//                }
-//            }
-//
-//            if ($useToken && ($tokenName == self::TOKEN_STRING || $tokenName == self::TOKEN_NS_SEPARATOR)) {
-//                $dependency .= $tokenVal;
-//            }
-//
-//            if ($namespaceIsBuilding && ($tokenName == self::TOKEN_STRING || $tokenName == self::TOKEN_NS_SEPARATOR)) {
-//                $result->namespace .= $tokenVal;
-//            }
-//
-//            if ($classNamePassed && $tokenName == self::TOKEN_STRING && empty($result->className)) {
-//                $result->className = $tokenVal;
-//            }
-//        }
-//
-//        return $result;
-//    }
 }
--- src/FileParserPhp8/FileParserPhp8.php
+++ src/FileParserPhp8/FileParserPhp8.php
@@ -7,12 +7,6 @@ use Brezgalov\ComponentsAnalyser\FileParser\Models\FileParseResult;
 
 class FileParserPhp8 extends FileParser
 {
-    const TOKEN_USE = "T_USE";
-    const TOKEN_NAME_QUALIFIED = "T_NAME_QUALIFIED";
-    const TOKEN_NAMESPACE = "T_NAMESPACE";
-    const TOKEN_CLASS = "T_CLASS";
-    const TOKEN_STRING = "T_STRING";
-
     /**
      * @return array
      */
@@ -22,77 +16,4 @@ class FileParserPhp8 extends FileParser
 
         ];
     }
-
-    /**
-     * @param string $filePath
-     * @return FileParseResult
-     */
-//    public function parseFile(string $filePath)
-//    {
-//        $result = new FileParseResult();
-//        $tokens = $this->tokenizeFile($filePath);
-//
-//        if ($tokens === false) {
-//            $result->error = "File \"{$filePath}\" not found or unavailable";
-//            return $result;
-//        }
-//
-//        if (empty($tokens)) {
-//            return $result;
-//        }
-//
-//        $useToken = false;
-//        $namespacePassed = false;
-//        $classNamePassed = false;
-//
-//        foreach ($tokens as $tokenInfo) {
-//            if (is_string($tokenInfo)) {
-//                continue;
-//            }
-//
-//            list($tokenCode, $tokenVal) = $tokenInfo;
-//            $tokenName = token_name($tokenCode);
-//
-//            switch ($tokenName) {
-//                case self::TOKEN_USE: {
-//                    $useToken = true;
-//                    continue 2;
-//                }
-//                case self::TOKEN_NAMESPACE: {
-//                    $namespacePassed = true;
-//                    continue 2;
-//                }
-//                case self::TOKEN_CLASS: {
-//                    $classNamePassed = true;
-//                    continue 2;
-//                }
-//            }
-//
-//            if ($useToken && $tokenName === self::TOKEN_NAME_QUALIFIED) {
-//                $useToken = false;
-//                $result->useClasses[] = $tokenVal;
-//                continue;
-//            }
-//
-//            if (
-//                $namespacePassed
-//                && empty($result->namespace)
-//                && $tokenName === self::TOKEN_NAME_QUALIFIED
-//            ) {
-//                $result->namespace = $tokenVal;
-//                continue;
-//            }
-//
-//            if (
-//                $classNamePassed
-//                && empty($result->className)
-//                && $tokenName === self::TOKEN_STRING
-//            ) {
-//                $result->className = $tokenVal;
-//                continue;
-//            }
-//        }
-//
-//        return $result;
-//    }
 }
